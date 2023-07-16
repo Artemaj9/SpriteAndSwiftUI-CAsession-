f859071485848888007a292dd7950b65165d93ee
Artem <moiseev@physics.msu.ru>
2023-07-16 12:20:54 +0300

    Initial Commit

---
+++ Emitter(CAsession)/Emitter_CAsession_App.swift
@@ -0,0 +1,17 @@
+//
+//  Emitter_CAsession_App.swift
+//  Emitter(CAsession)
+//
+//  Created by Artem on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Emitter_CAsession_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
