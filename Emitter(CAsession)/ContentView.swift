a05317e2321132752d4a8a7e64ad1df3add09ba4
Artem <moiseev@physics.msu.ru>
2023-07-16 14:43:10 +0300

    SpriteKit+UIView

--- Emitter(CAsession)/ContentView.swift
+++ Emitter(CAsession)/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+       super.didMove(to: view)
+        
+    setupParticleEmitter()
+    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first else
+//        { return }
+//        let location = touch.location(in: self)
+//
+//
+//        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+//        box.position = location
+//        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+//        addChild(box)
+     
+        func setupParticleEmitter() {
+            let particleEmitter = SKEmitterNode(fileNamed: "Snow")
+            particleEmitter?.position = CGPoint(x: size.width / 2, y: size.height - 50)
+            addChild(particleEmitter!)
+        }
+    }
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  
+        var scene : SKScene {
+            let scene = GameScene()
+            scene.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            scene.scaleMode = .aspectFill
+            return scene
         }
-        .padding()
+        var body: some View {
+            ZStack {
+            SpriteView(scene: scene)
+                    
+              //  .frame(width: proxy.size.width, height: proxy.size.height)
+            
+            Text("Wonderland!")
+                    .foregroundColor(.white.opacity(0.8))
+                    .font(.largeTitle)
+                    .bold()
+        }
+            .ignoresSafeArea()
     }
 }
 
